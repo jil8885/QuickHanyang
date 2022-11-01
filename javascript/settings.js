f1b8b615545245b25435322f73c72564fe9f3219
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-01 18:06:22 +0900

    feat: 자신의 학과를 선택할 수 있게 옵션 구현

--- javascript/settings.js
+++ javascript/settings.js
@@ -1,4 +1,5 @@
 const themeList = ["Light", "Dark"];
+const majorList = ['소프트웨어학부', 'ICT융합학부', '인공지능학과', '기계공학과', '로봇공학과', '산업경영공학과', '전자공학부', '재료화학공학과', '생명나노공학과', '건축학부', '건설환경플랜트공학과', '교통물류공학과', '국방정보공학과', '융합공학과', '스마트융합공학부', '약학과', '응용수학과', '응용물리학과', '화학분자공학 과', '분자생명과학과', '해양융합공학과', '나노광전자학과', '한국언어문학과', '문화인류학과', '문화콘텐츠학과', '중국학과', '일본학과', '영미언어문화학과', '프랑스학 과', '정보사회미디어학과', '광고홍보학과', '경제학부', '경영학부', '보험계리학과', '회계세무학과', '주얼리·패션디자인학과', '산업디자인학과', '커뮤니케이션디자인학과', '영상디자인학과', '스포츠과학부', '무용예술학과', '실용음악학과']
 
 window.addEventListener('load', function() {
     let themeColor = document.getElementById('theme-color-button');
@@ -12,4 +13,31 @@ window.addEventListener('load', function() {
         });
         themeColor.appendChild(selectThemeButton);
     });
+
+    let selectMajorElement = document.getElementById('select-major-button');
+    chrome.storage.sync.get('major', function(data) {
+        majorList.forEach(function(major) {
+            let majorSelectButton = document.createElement('option');
+            majorSelectButton.textContent = major;
+            majorSelectButton.value = major;
+            if (data.major === major) {
+                majorSelectButton.selected = true;
+            }
+            selectMajorElement.appendChild(majorSelectButton);
+        });
+    });
+    let button = document.createElement('button');
+    button.id = "save-major-button";
+    button.textContent = "저장";
+    button.addEventListener('click', function() {
+        if(selectMajorElement.selectedIndex === 0){
+            alert("학과를 선택해주세요.");
+            return;
+        }
+        let data = selectMajorElement.options[selectMajorElement.selectedIndex].value;
+        chrome.storage.sync.set({major: data}, function() {
+            window.alert(data + "로 변경 완료!");
+        });
+    });
+    selectMajorElement.parentNode.appendChild(button);
 });
