b39cb0d08baef6bf00fa6b4469320cf7f61c296e
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-11-02 15:53:33 +0900

    feat: 사용자 커스터마이징 추가 (#1)

---
+++ javascript/index.js
@@ -0,0 +1,125 @@
+window.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.sync.get('theme', function(data) {
+        for (let i = 1; i <= 9; i++) {
+            createPageButton(document.getElementById(`menu-button-${i}`), i, data.theme);
+        }
+        if (data.theme === 'Dark') {
+            document.body.style.backgroundColor = '#1e1e1e';
+            document.getElementById("settingsButtonImage").style.filter = "invert(100%)";
+            document.getElementById("repositoryButtonImage").style.filter = "invert(100%)";
+            document.getElementsByClassName("grid-container")[0].style.backgroundColor = "#1e1e1e";
+
+            let buttons = document.getElementsByClassName("menu-button");
+
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].style.borderColor = "#7f7f7f";
+                // change the color of the button's border when hovered over
+                buttons[i].onmouseover = function() {
+                    buttons[i].style.borderColor = "#ffffff";
+                }
+                buttons[i].onmouseout = function() {
+                    buttons[i].style.borderColor = "#7f7f7f";
+                }
+            }
+            let footer = document.getElementsByClassName("footer")[0];
+            footer.style.color = "#ffffff"
+            footer.getElementsByTagName("a")[0].style.color = "#ffffff";
+            footer.getElementsByTagName("a")[1].style.color = "#ffffff";
+        }
+    });
+})
+
+function createPageButton(element, buttonIndex, theme) {
+    chrome.storage.sync.get(`Page${buttonIndex}`, function(data) {
+        let pageName = data[`Page${buttonIndex}`]
+        if (pageName) {
+            fetch("../data/pages.json")
+                .then(response => response.json())
+                .then(data => {
+                    let page = data[pageName];
+                    let url = ""
+                    if (pageName === '나의 학과'){
+                        chrome.storage.sync.get('major', function(data) {
+                            let majorName = data.major;
+                            fetch("../data/major.json")
+                                .then(response => response.json())
+                                .then(json => {
+                                    let major = json[majorName];
+                                    if (major) {
+                                        url = major.major;
+                                        setPageButtonElement(element, pageName, url, major.image, theme);
+                                    }
+                                });
+                        });
+                    } else if (pageName === '나의 단과대학'){
+                        chrome.storage.sync.get('major', function(data) {
+                            let majorName = data.major;
+                            fetch("../data/major.json")
+                                .then(response => response.json())
+                                .then(json => {
+                                    let major = json[majorName];
+                                    if (json) {
+                                        url = major.college;
+                                        setPageButtonElement(element, pageName, url, major.image, theme);
+                                    }
+                                });
+                        });
+                    } else {
+                        url = page.url;
+                        let image = page.image;
+                        setPageButtonElement(element, pageName, url, image, theme);
+                    }
+                }
+            )
+        } else {
+            switch (buttonIndex) {
+                case 1:
+                    setPageButtonElement(element, "홈페이지", "https://www.hanyang.ac.kr/web/www/home", "../images/icons/img_homepage.png", theme);
+                    break;
+                case 2:
+                    setPageButtonElement(element, "포털", "https://portal.hanyang.ac.kr", "../images/icons/img_portal.png", theme);
+                    break;
+                case 3:
+                    setPageButtonElement(element, "LMS", "https://lms.hanyang.ac.kr", "../images/icons/img_lms.png", theme);
+                    break;
+                case 4:
+                    setPageButtonElement(element, "수강신청", "https://sugang.hanyang.ac.kr", "../images/icons/img_class.png", theme);
+                    break;
+                case 5:
+                    setPageButtonElement(element, "학사일정", "https://www.hanyang.ac.kr/web/www/cal_academic", "../images/icons/img_calendar.png", theme);
+                    break;
+                case 6:
+                    setPageButtonElement(element, "학술정보관", "https://information.hanyang.ac.kr/", "../images/icons/img_library.png", theme);
+                    break;
+                case 7:
+                    setPageButtonElement(element, "메일", "https://mail.google.com/", "../images/icons/img_class.png", theme);
+                    break;
+                case 8:
+                    setPageButtonElement(element, "전자출결", "https://check.hanyang.ac.kr", "../images/icons/img_class.png", theme);
+                    break;
+                case 9:
+                    setPageButtonElement(element, "오류 및 건의", "https://open.kakao.com/o/sW2kAinb", "../images/icons/img_calendar.png", theme);
+                    break;
+            }
+        }
+    });
+}
+
+function setPageButtonElement(element, linkName, URL, IMG, theme) {
+    onSettingsButtonClick(element, URL);
+    let buttonImage = document.createElement("img");
+    let buttonLabel = document.createElement("p");
+    buttonImage.src = IMG;
+    buttonLabel.textContent = linkName;
+    if (theme === 'Dark') {
+        buttonLabel.style.color = "#ffffff";
+    }
+    element.appendChild(buttonImage);
+    element.appendChild(buttonLabel);
+}
+
+function onSettingsButtonClick(element, url) {
+    element.addEventListener('click', function() {
+        window.open(url);
+    });
+}
---
+++ javascript/settings.js
@@ -0,0 +1,78 @@
+const themeList = ["Light", "Dark"];
+const majorList = ['소프트웨어학부', 'ICT융합학부', '인공지능학과', '기계공학과', '로봇공학과', '산업경영공학과', '전자공학부', '재료화학공학과', '생명나노공학과', '건축학부', '건설환경플랜트공학과', '교통물류공학과', '국방정보공학과', '융합공학과', '스마트융합공학부', '약학과', '응용수학과', '응용물리학과', '화학분자공학과', '분자생명과학과', '해양융합공학과', '나노광전자학과', '한국언어문학과', '문화인류학과', '문화콘텐츠학과', '중국학과', '일본학과', '영미언어문화학과', '프랑스학과', '정보사회미디어학과', '광고홍보학과', '경제학부', '경영학부', '보험계리학과', '회계세무학과', '주얼리·패션디자인학과', '산업디자인학과', '커뮤니케이션디자인학과', '영상디자인학과', '스포츠과학부', '무용예술학과', '실용음악학과']
+const pageList =  ["홈페이지", "포털", "LMS", "수강신청", "학사일정", "전자출결", "학생식당", "교직원식당", "푸드코트", "기숙사식당", "창업보육센터", "나의 단과대학", "나의 학과", "학술정보관", "오류 및 건의"];
+
+window.addEventListener('load', function() {
+    let themeColor = document.getElementById('theme-color-button');
+    themeList.forEach(function(theme) {
+        let selectThemeButton = document.createElement('button');
+        selectThemeButton.textContent = theme;
+        selectThemeButton.addEventListener('click', function() {
+            chrome.storage.sync.set({theme: theme}, function() {
+                console.log('Theme set to ' + theme);
+            });
+        });
+        themeColor.appendChild(selectThemeButton);
+    });
+
+    let selectMajorElement = document.getElementById('select-major-button');
+    chrome.storage.sync.get('major', function(data) {
+        majorList.forEach(function(major) {
+            let majorSelectButton = document.createElement('option');
+            majorSelectButton.textContent = major;
+            majorSelectButton.value = major;
+            if (data.major === major) {
+                majorSelectButton.selected = true;
+            }
+            selectMajorElement.appendChild(majorSelectButton);
+        });
+    });
+    let button = document.createElement('button');
+    button.id = "save-major-button";
+    button.textContent = "저장";
+    button.addEventListener('click', function() {
+        if(selectMajorElement.selectedIndex === 0){
+            alert("학과를 선택해주세요.");
+            return;
+        }
+        let data = selectMajorElement.options[selectMajorElement.selectedIndex].value;
+        chrome.storage.sync.set({major: data}, function() {
+            window.alert(data + "로 변경 완료!");
+            window.location.reload();
+        });
+    });
+    selectMajorElement.parentNode.appendChild(button);
+    for(let i = 1; i <= 9; i++){
+        insertPageButton(i);
+    }
+});
+
+function insertPageButton(itemPosition){
+    let element = document.getElementById(`button-link-${itemPosition}`);
+    pageList.forEach(function(page) {
+        let pageButton = document.createElement('button');
+        pageButton.textContent = page;
+        pageButton.className = "page-button";
+        if (page === "나의 학과" || page === "나의 단과대학") {
+            chrome.storage.sync.get('major', function(data) {
+                let majorName = data.major;
+                fetch("../data/major.json")
+                    .then(response => response.json())
+                    .then(json => {
+                        let major = json[majorName];
+                        if (major["major"].length === 0 && page === "나의 학과") {
+                            pageButton.disabled = true;
+                        } else if (major["college"].length === 0 && page === "나의 단과대학") {
+                            pageButton.disabled = true;
+                        }
+                    });
+            });
+        }
+        pageButton.addEventListener('click', function() {
+            chrome.storage.sync.set({[`Page${itemPosition}`]: page}, function() {
+                console.log('Page set to ' + page);
+            });
+        });
+        element.appendChild(pageButton);
+    });
+}
